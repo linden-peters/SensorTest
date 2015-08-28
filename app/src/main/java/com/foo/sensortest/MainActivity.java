9940130901a9dcd48be188273b99be1145becf9a
linden-peters <linden.peters@gmail.com>
2015-08-28 13:55:53 -0600

    Action Menu
    Decided to put menu functions back in after all.

--- app/src/main/java/com/foo/sensortest/MainActivity.java
+++ app/src/main/java/com/foo/sensortest/MainActivity.java
@@ -1,11 +1,14 @@
 package com.foo.sensortest;
 
+import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 import java.util.Calendar;
 import java.util.List;
@@ -76,8 +79,8 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         mySensor.setText(Calendar.getInstance().getTime()
                 + "\n\t|\t\t" + (event.values[1] < -2 ? "A" : ".") + "\t\t|\t" + event.values[0]
                 + "\n\t|\t" + (event.values[0] > 2 ? "<" : ".") + "\t"
-                + (event.values[2] > 2 ? "X" : (event.values[2] < -2 ? "O" : ".") )
-                    + "\t" + (event.values[0] < -2 ? ">" : ".") + "\t|\t" + event.values[1]
+                    + (event.values[2] > 2 ? "X" : (event.values[2] < -2 ? "O" : ".") )
+                        + "\t" + (event.values[0] < -2 ? ">" : ".") + "\t|\t" + event.values[1]
                 + "\n\t|\t\t" + (event.values[1] > 2 ? "V" : ".") + "\t\t|\t" + event.values[2]);
     }
 
@@ -91,5 +94,31 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     protected void onPause() {
         super.onPause();
         sm.unregisterListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        else if (id == R.id.action_about) {
+            startActivity(new Intent(this, AboutActivity.class));
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
